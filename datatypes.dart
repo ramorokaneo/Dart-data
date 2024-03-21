ff770d6fef907576ff2b30d15211e7cf64eae5d0
ramorokaneo <ramoroka.neo@gmail.com>
2024-03-21 15:12:35 +0200

    first commit

---
+++ datatypes.dart
@@ -0,0 +1,23 @@
+// Dart program demonstrating data types
+
+void main() {
+  // int data type
+  int age = 30;
+  print('Age: $age');
+
+  // double data type
+  double height = 5.11;
+  print('Height: $height');
+
+  // String data type
+  String name = 'Neo Ramoroka';
+  print('Name: $name');
+
+  // List data type
+  List<String> fruits = ['Apple', 'Banana', 'Orange'];
+  print('Fruits: $fruits');
+
+  // Map data type
+  Map<String, int> marks = {'Math': 90, 'Science': 85, 'English': 88};
+  print('Marks: $marks');
+}
